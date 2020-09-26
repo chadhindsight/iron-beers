4d20c4195df3a3ba67bd3b72722f41f1e9e841b7
Chad Hinds <chadrickj8@gmail.com>
2020-09-26 17:52:24 -0400

    Hooks and ting

--- starter-code/src/components/Beers.jsx
+++ starter-code/src/components/Beers.jsx
@@ -1,27 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Beers extends Component {
-
-  showBeers = () => {
-    return this.props.beers.map(eachBeer => {
+function Beers(props) {
+  const showBeers = () => {
+    return props.beers.map((eachBeer) => {
       return (
         <div key={eachBeer._id}>
-          <img src={eachBeer.image_url}/>
+          <img src={eachBeer.image_url} />
           <h3>{eachBeer.name}</h3>
           <h6>{eachBeer.tagline}</h6>
           <p>Contributed by: {eachBeer.contributed_by}</p>
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
-  render() {
-    console.log(this.props.beers)
-    return (
-      <div>
-        {this.props.dataReady?(this.showBeers()): ("Loading...")}
-        {/* {this.showBeers()} */}
-      </div>
-    )
-  }
+  console.log(props.beers);
+  return (
+    <div>
+      {props.dataReady ? showBeers() : "Loading..."}
+      {/* {showBeers()} */}
+    </div>
+  );
 }
+export default Beers;
