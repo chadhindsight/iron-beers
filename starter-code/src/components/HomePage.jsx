db3dab931d30b945ff7fd390ad7f0add6292c445
Chad Hinds <chadrickj8@gmail.com>
2020-09-26 19:04:08 -0400

    Remove uneeded files

--- starter-code/src/components/HomePage.jsx
+++ starter-code/src/components/HomePage.jsx
@@ -1,15 +1,58 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class HomePage extends Component {
-  
   render() {
     return (
-      <ul>
-        <li><Link to="/beers">Beers</Link></li>
-        <li><Link to="/random-beer">Random Beer</Link></li>
-        <li><Link to="/new-beer">New Beer</Link></li>
-      </ul>
-    )
+      <>
+        <ul>
+          <li>
+            <Link to="/beers">
+              Beers
+              <img
+                src="https://images.unsplash.com/photo-1575037614876-c38a4d44f5b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                alt="whatever"
+              />
+            </Link>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </li>
+          <li>
+            <Link to="/random-beer">
+              Random Beer
+              <img
+                src="https://images.unsplash.com/photo-1549044466-404f19605ddc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                alt="drinks"
+              />
+            </Link>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </li>
+          <li>
+            <Link to="/new-beer">
+              New Beer
+              <img
+                src="https://images.unsplash.com/photo-1516458464372-eea4ab222b31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                alt="good stuff"
+              />
+            </Link>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </li>
+        </ul>
+      </>
+    );
   }
 }
