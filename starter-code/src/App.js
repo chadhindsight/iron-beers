1c09c58982a59f4438f3fc24ff29fdfcb90b5175
Chad Hinds <chadrickj8@gmail.com>
2020-09-26 18:21:13 -0400

    Implement useState and useEffect instead of traditional state

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-import {Switch, Route, Link} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Axios from 'axios';
 import Beers from './components/Beers';
@@ -10,37 +10,31 @@ import Beers from './components/Beers';
 let baseUrl = "https://ih-beers-api2.herokuapp.com/beers"
 let allBeersEndpoint = '/'
 
-class App extends Component {
-  state = {
-    dataReady: false
+function App() {
+  const [dataReady, setDataReady] = useState(false);
+  const [beers, setBeers] = useState([])
+
+  useEffect(() => {
+    getAllBeers()
+  }, [])
+
+  const getAllBeers = () => {
+    Axios.get(baseUrl + allBeersEndpoint)
+      .then(response => {
+        setBeers(response.data)// save the array of the data instead of the whole object response
+        setDataReady(true)
+      }).catch(err => console.log(err))
   }
 
-  componentDidMount = () => {
-    this.getAllBeers()
-  }
-
-  getAllBeers = () => {
-    Axios.get(baseUrl+allBeersEndpoint)
-    .then(response =>{
-      this.setState({
-        beers: response.data, // save the array of the data instead of the whole object response
-        dataReady: true
-      })
-    })
-    .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <div>
-        <Header/>
-        <Switch>
-          <Route exact path='/home' render={props => <HomePage {...props}/>}/>
-          <Route exact path='/beers' render={props => <Beers {...props} beers={this.state.beers} dataReady={this.state.dataReady}/>}/>
-        </Switch>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/home' render={props => <HomePage {...props} />} />
+        <Route exact path='/beers' render={props => <Beers {...props} beers={beers} dataReady={dataReady} />} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
