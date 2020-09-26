2926ca2b09ca7e7e2ea12e04ae0dd287acb19a06
Chad Hinds <chadrickj8@gmail.com>
2020-09-26 18:41:51 -0400

    Display all sections

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Axios from 'axios';
 import Beers from './components/Beers';
 
-let baseUrl = "https://ih-beers-api2.herokuapp.com/beers"
-let allBeersEndpoint = '/'
+let baseUrl = "https://ih-beers-api2.herokuapp.com/beers/"
 
 function App() {
   const [dataReady, setDataReady] = useState(false);
@@ -19,7 +18,7 @@ function App() {
   }, [])
 
   const getAllBeers = () => {
-    Axios.get(baseUrl + allBeersEndpoint)
+    Axios.get(baseUrl)
       .then(response => {
         setBeers(response.data)// save the array of the data instead of the whole object response
         setDataReady(true)
@@ -30,7 +29,7 @@ function App() {
     <div>
       <Header />
       <Switch>
-        <Route exact path='/home' render={props => <HomePage {...props} />} />
+        <Route exact path='/' render={props => <HomePage {...props} />} />
         <Route exact path='/beers' render={props => <Beers {...props} beers={beers} dataReady={dataReady} />} />
       </Switch>
     </div>
