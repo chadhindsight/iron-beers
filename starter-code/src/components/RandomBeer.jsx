dc3c2dfadfc6b2e6fd0e50d533ba93a45690318e
Chad Hinds <chadrickj8@gmail.com>
2020-09-28 20:08:37 -0400

    DO it old school way with state

--- starter-code/src/components/RandomBeer.jsx
+++ starter-code/src/components/RandomBeer.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-// Display a random beer on /random-beer route
-const RandomBeer = (props) => {
-  return <div></div>;
-};
+import React, { Component } from "react";
+import axios from "axios";
 
-export default RandomBeer;
+export default class Beer extends Component {
+  state = { beer: null };
+  async componentDidMount() {
+    const beer = await axios.get(
+      "https://ih-beers-api2.herokuapp.com/beers/random"
+    );
+    console.log(beer);
+    this.setState({
+      beer: beer.data,
+    });
+  }
+
+  render() {
+    // console.log(this.props);
+    return this.state.beer ? (
+      <div>
+        <img src={this.state.beer.image_url} alt="Our beer" />
+        <h3>{this.state.beer.name}</h3>
+        <p>{this.state.beer.tagline}</p>
+        <br />
+        <p> {this.state.beer.description}</p>
+      </div>
+    ) : (
+      <div>
+        {" "}
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+}
